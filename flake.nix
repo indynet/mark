e49965b0f0a72e10a6b93d3cf784e30ec898139b
indy <indynet00@gmail.com>
2025-11-04 23:58:08 -0600

    mk:initial commit

---
+++ flake.nix
@@ -0,0 +1,114 @@
+{
+  description = "mark";
+
+  inputs      = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs     = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs   = import nixpkgs { inherit system; };
+
+      writer = rec {
+        bind   = m: f: let r = f m.value; in { value = r.value; write = m.write + r.write; };
+        apply  = wf: wa: { value = wf.value wa.value; write = wf.write + wa.write; };
+        pass   = m: { value = m.value.value; write = m.value.func m.write; };
+        liftA  = f: m: { value = f m.value; write = m.write; };
+        pure   = x: { value = x; write = ""; };
+        listen = m: { write = m.write; value = m; };
+        tell   = w: { write = w; value = null; };
+        and    = m: m': bind m (_: m');
+      };
+    in
+      with builtins;
+      with pkgs.lib;
+      with pkgs;
+    rec {
+      monad = let lift = m: _: m; in
+              rec {
+                bind   = m: f: r: writer.bind (m r) (a: (f a) r);
+                apply  = rf: ra: r: writer.apply (rf r) (ra r);
+                first  = l: r: bind l (x: and r (pure x));
+                do     = o: foldl bind (pure null) o;
+                listen = m: lift (writer.listen m);
+                tell   = w: lift (writer.tell w);
+                pass   = m: lift (writer.pass m);
+                and    = m: m': bind m (_: m');
+                liftA  = f: m: writer.liftA f;
+                pure   = x: _: writer.pure x;
+                local  = m: f: r: m (f r);
+                ask    = r: writer.pure r;
+                void   = m: _: m;
+              };
+
+      attrs = attr:
+        let
+          f = x: ''${x}=\"${attr.${x}}\"'';
+          g = map f (attrNames attr);
+          h = concatStringSep " " g;
+        in h;
+
+      one   = name: attr: m:
+        if length (attrNames attr) > 0
+        then monad.and (monad.tell "<${name} ${attrs attr}>") m
+        else monad.and (monad.tell "<${name}>") m;
+
+      scl   = name: attr: one name attr (pure null);
+
+      tag   = name: attr: m:
+        let
+          f = monad.tell "</${name}>";
+          g = x: monad.first (monad.and (monad.tell x) m) f;
+        in
+          if length (attrNames attr) > 0
+          then g "<${name} ${attrs attr}>"
+          else g "<${name}>";
+
+      html = {
+        footer = tag "footer";
+        header = tag "header";
+        tbody  = tag "tbody";
+        title  = tag "title";
+        table  = tag "table";
+        link   = scl "link";
+        meta   = scl "meta";
+        head   = tag "head";
+        span   = tag "span";
+        body   = tag "body";
+        main   = tag "main";
+        html   = tag "html";
+        div    = tag "div";
+        img    = tag "img";
+        doc    = one "!DOCTYPE html" {};
+        h1     = tag "h1";
+        h2     = tag "h2";
+        h3     = tag "h3";
+        h4     = tag "h4";
+        h5     = tag "h5";
+        h6     = tag "h6";
+        ul     = tag "ul";
+        ol     = tag "ol";
+        li     = tag "li";
+        td     = tag "td";
+        tr     = tag "tr";
+        p      = tag "p";
+        b      = tag "b";
+        i      = tag "i";
+        a      = tag "a";
+      };
+
+      packages.${system}.default =
+        let
+          text = with monad; (tell "a");
+        in stdenv.mkDerivation {
+          name         = "test";
+          src          = self;
+
+          installPhase = ''
+              mkdir -p $out
+              echo "${(text 0).write}" > $out/text.txt
+          '';
+        };
+    };
+}
